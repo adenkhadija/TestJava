c316b32776cf878e804f09866877be59978074c5
Khadija <adenkhadijaa@gmail.com>
2023-02-08 08:37:09 -0600

    first commit

---
+++ palindrom.java
@@ -0,0 +1,21 @@
+public class palindrom {
+    public static void main(String[] args) {
+        
+        int a = 10;   
+        String b = Integer.toString(a) ; // int to string 
+        System.out.println("the string is: " + b); 
+
+        String c = String.valueOf(a) ; 
+        // String valueOf() method converts different types 
+        // of values into string 
+        System.out.println("The value is: "+ c);
+        System.out.println();
+        
+
+        
+
+
+    }
+    
+    
+}
